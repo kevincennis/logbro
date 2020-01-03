c24c6504f3a5dca94f5d3993b08015e56f7141e4
Max Greenwald <mgreenwald@starry.com>
2020-01-03 14:25:30 -0500

    Add tests for Logbro class

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 'use strict';
 
-module.exports = require('./lib/logbro');
+const Logbro = require('./lib/logbro');
+
+// Set the default global level and format
+Logbro.level = process.env.NODE_DEBUG;
+Logbro.format = process.env.LOGBRO_FORMAT;
+
+module.exports = new Logbro();
+module.exports.Logbro = Logbro;
